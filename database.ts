d30a7f3c5ade123e30c9e4606ae55d4d03204baa
spaniard95 <panagiotis.spanos95@gmail.com>
2024-10-25 20:14:28 +0300

    remove stupid fix for db

--- database.ts
+++ database.ts
@@ -1,6 +1,7 @@
 import { neon } from "@neon/serverless";
 
 const databaseUrl = Deno.env.get("DATABASE_URL");
+console.log("DATABASE_URL", databaseUrl);
 
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is not set in the environment variables");
