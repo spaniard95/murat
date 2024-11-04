b27674ab958b397e442f793d36137a2a705d2835
spaniard95 <panagiotis.spanos95@gmail.com>
2024-11-04 22:43:25 +0200

    fix validators for dates

--- lib/validators/dateValidators.ts
+++ lib/validators/dateValidators.ts
@@ -1,8 +1,8 @@
 export const isValidDay = (day: string) =>
-  !(Number(day) >= 1 && Number(day) <= 31);
+  Number(day) >= 1 && Number(day) <= 31;
 
 export const isValidMonth = (month: string) =>
-  !(Number(month) >= 1 && Number(month) <= 12);
+  Number(month) >= 1 && Number(month) <= 12;
 
 export const isValidYear = (year: string) =>
-  !(Number(year) >= 1900 && Number(year) <= 2100);
+  Number(year) >= 1900 && Number(year) <= 2100;
--- services/types.ts
+++ services/types.ts
@@ -1,5 +1,5 @@
 export interface Expense {
-  id?: number; // TODO: in the response is i have the id but on the request i dont need it
+  id?: number; // TODO: in the response i have the id but on the request i dont need it
   category: string;
   subcategory: string;
   amount: number;
