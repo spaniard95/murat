1aa9b3e198a691f8ed1c0df775fc14fa2a7f2707
spaniard95 <panagiotis.spanos95@gmail.com>
2024-11-04 22:35:27 +0200

    fix validators

--- lib/validators/dateValidators.ts
+++ lib/validators/dateValidators.ts
@@ -1,8 +1,8 @@
 export const isValidDay = (day: string) =>
-  isNaN(Number(day)) || Number(day) < 1 || Number(day) > 31;
+  !(Number(day) >= 1 && Number(day) <= 31);
 
 export const isValidMonth = (month: string) =>
-  isNaN(Number(month)) || Number(month) < 1 || Number(month) > 12;
+  !(Number(month) >= 1 && Number(month) <= 12);
 
 export const isValidYear = (year: string) =>
-  isNaN(Number(year)) || Number(year) < 1815 || Number(year) > 3000;
+  !(Number(year) >= 1900 && Number(year) <= 2100);
