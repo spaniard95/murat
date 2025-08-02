16d5641a7b3e0475fbfdc62a48ff1e19f5c87e94
spaniard95 <panagiotis.spanos95@gmail.com>
2025-08-02 11:31:53 +0300

    allow all temp

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins for development purposes
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
